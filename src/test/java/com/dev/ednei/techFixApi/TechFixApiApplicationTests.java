88d66d2ad0a292b8d8c198b33fcd99b940cc859c
Ednei Gonzaga <edneigonzaga444@gmail.com>
2026-04-23 19:17:48 -0300

    initial

---
+++ src/test/java/com/dev/ednei/techFixApi/TechFixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ednei.techFixApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechFixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

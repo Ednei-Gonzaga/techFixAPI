c3e4162c6f70fbf2bce0dc47deda0100debae3d5
Ednei Gonzaga <edneigonzaga444@gmail.com>
2026-07-08 18:24:05 -0300

    feat: Added findMostUsedPart and findLeastUsedPart  in ServiceOrderItemRepository

--- backend/src/main/java/com/dev/ednei/techFixApi/repository/ServiceOrderItemRepository.java
+++ backend/src/main/java/com/dev/ednei/techFixApi/repository/ServiceOrderItemRepository.java
@@ -1,10 +1,13 @@
 package com.dev.ednei.techFixApi.repository;
 
+import com.dev.ednei.techFixApi.DTOS.analytics.dataInDashboardFormat.MostUsedPartFormat;
 import com.dev.ednei.techFixApi.DTOS.serviceOrderItem.ServiceOrderItemFullDTO;
 import com.dev.ednei.techFixApi.model.ServiceOrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ServiceOrderItemRepository extends JpaRepository<ServiceOrderItem, Long> {
@@ -12,4 +15,32 @@ public interface ServiceOrderItemRepository extends JpaRepository<ServiceOrderIt
     boolean existsByServiceOrderIdAndPartId(Long serviceOrderId, Long partId);
 
     List<ServiceOrderItem> findAllByServiceOrderId(Long serviceOrderId);
+
+    @Query("""
+        SELECT item.namePart, SUM(item.quantity)
+        FROM ServiceOrderItem item
+        JOIN item.serviceOrder so
+        WHERE so.dateTimeStart BETWEEN :dateStart AND :dateEnd
+        AND so.status IN ('COMPLETED', 'DELIVERED')
+        GROUP BY item.part, item.namePart
+        ORDER BY SUM(item.quantity) DESC LIMIT 3
+        """)
+    List<MostUsedPartFormat> findMostUsedPart(
+            @Param("dateStart") LocalDateTime dateStart,
+            @Param("dateEnd") LocalDateTime dateEnd
+    );
+
+    @Query("""
+        SELECT item.namePart, SUM(item.quantity)
+        FROM ServiceOrderItem item
+        JOIN item.serviceOrder so
+        WHERE so.dateTimeStart BETWEEN :dateStart AND :dateEnd
+        AND so.status IN ('COMPLETED', 'DELIVERED')
+        GROUP BY item.part, item.namePart
+        ORDER BY SUM(item.quantity) ASC LIMIT 3
+        """)
+    List<MostUsedPartFormat> findLeastUsedPart(
+            @Param("dateStart") LocalDateTime dateStart,
+            @Param("dateEnd") LocalDateTime dateEnd
+    );
 }

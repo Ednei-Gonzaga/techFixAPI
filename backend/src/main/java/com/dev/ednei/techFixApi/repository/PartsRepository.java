22640517a51c6df365d5de718cafa1cf9f3cd6e8
Ednei Gonzaga <edneigonzaga444@gmail.com>
2026-07-10 22:59:33 -0300

    feat: added low stock alert (red and yellow) to Part

--- backend/src/main/java/com/dev/ednei/techFixApi/repository/PartsRepository.java
+++ backend/src/main/java/com/dev/ednei/techFixApi/repository/PartsRepository.java
@@ -1,5 +1,6 @@
 package com.dev.ednei.techFixApi.repository;
 
+import com.dev.ednei.techFixApi.DTOS.analytics.dataDashboardAlertsFormat.StockAlertsDTO;
 import com.dev.ednei.techFixApi.model.Parts;
 import jakarta.mail.Part;
 import org.springframework.data.domain.Page;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PartsRepository extends JpaRepository<Parts, Long> {
@@ -29,4 +31,45 @@ public interface PartsRepository extends JpaRepository<Parts, Long> {
                 OR p.codeSku ILIKE %:part%  
            """)
     Page<Parts> findAllByNameOrCodeSkuOrStatus(@Param("status") List<Boolean> status, @Param("part") String part, Pageable pageable);
+
+    @Query("""
+        SELECT p.id, p.name, p.stockQuantity, 
+               SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartPreviousMonth AND :dateTimeEndPreviousMonth THEN item.quantity ELSE 0 END),
+               SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartCurrentMonth AND :dateTimeEndCurrentMonth THEN item.quantity ELSE 0 END)
+        FROM Parts p
+        LEFT JOIN ServiceOrderItem item ON item.part.id = p.id
+        LEFT JOIN ServiceOrder so ON item.serviceOrder.id = so.id 
+        GROUP BY p.name, p.id
+        HAVING SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartPreviousMonth AND :dateTimeEndPreviousMonth THEN item.quantity ELSE 0 END) 
+               >= (p.stockQuantity + SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartCurrentMonth AND :dateTimeEndCurrentMonth THEN item.quantity ELSE 0 END))
+        OR p.stockQuantity <= 2
+                """)
+        List<StockAlertsDTO> findLowStockAlertsRed(
+                @Param("dateTimeStartPreviousMonth") LocalDateTime dateTimeStartPreviousMonth,
+                @Param("dateTimeEndPreviousMonth") LocalDateTime dateTimeEndPreviousMonth,
+                @Param("dateTimeStartCurrentMonth") LocalDateTime  dateTimeStartCurrentMonth,
+                @Param("dateTimeEndCurrentMonth") LocalDateTime dateTimeEndCurrentMonth
+    );
+
+
+    @Query("""
+            SELECT p.id, p.name, p.stockQuantity, 
+                   SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartPreviousMonth AND :dateTimeEndPreviousMonth THEN item.quantity ELSE 0 END),
+                   SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartCurrentMonth AND :dateTimeEndCurrentMonth THEN item.quantity ELSE 0 END)
+            FROM Parts p
+            LEFT JOIN ServiceOrderItem item ON item.part.id = p.id
+            LEFT JOIN ServiceOrder so ON item.serviceOrder.id = so.id 
+            GROUP BY p.name, p.id
+            HAVING SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartPreviousMonth AND :dateTimeEndPreviousMonth THEN item.quantity ELSE 0 END) 
+                   < (p.stockQuantity + SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartCurrentMonth AND :dateTimeEndCurrentMonth THEN item.quantity ELSE 0 END))
+            AND ((p.stockQuantity + SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartCurrentMonth AND :dateTimeEndCurrentMonth THEN item.quantity ELSE 0 END))
+            - SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartPreviousMonth AND :dateTimeEndPreviousMonth THEN item.quantity ELSE 0 END))
+            <  SUM(CASE WHEN so.dateTimeStart BETWEEN :dateTimeStartPreviousMonth AND :dateTimeEndPreviousMonth THEN item.quantity ELSE 0 END)
+            """)
+    List<StockAlertsDTO> findLowStockAlertsYellow(
+            @Param("dateTimeStartPreviousMonth") LocalDateTime dateTimeStartPreviousMonth,
+            @Param("dateTimeEndPreviousMonth") LocalDateTime dateTimeEndPreviousMonth,
+            @Param("dateTimeStartCurrentMonth") LocalDateTime  dateTimeStartCurrentMonth,
+            @Param("dateTimeEndCurrentMonth") LocalDateTime dateTimeEndCurrentMonth
+    );
 }

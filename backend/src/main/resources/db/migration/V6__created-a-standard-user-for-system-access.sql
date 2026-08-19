ee614525c51968fcc3f79c60f62f8f9afa86bfc0
Ednei Gonzaga <edneigonzaga444@gmail.com>
2026-08-19 10:51:07 -0300

    feat: created a standard user

---
+++ backend/src/main/resources/db/migration/V6__created-a-standard-user-for-system-access.sql
@@ -0,0 +1,3 @@
+insert into users values(1,'UserAdmin', '$2a$12$ui0kiaphzJDN5qkCtVpuqO4tInR98FR/3HhswL8Q4soBdjMoz5j02', 'true', null, now() , null, 'MANAGER', 'false');
+
+insert into employees values(1, 'user_default', 'UserAdmin','61999887766', '61999887766','techFix@example.com', 1);
